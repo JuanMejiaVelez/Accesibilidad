42784d44f09034c896d007b494359e8e196d3e27
Juan Mejia <jumejia@microsoft.com>
2019-02-10 17:15:03 -0800

    Proyecton Navegacion por Teclado, inicio

---
+++ Teclado/Teclado/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teclado
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void StepOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            StepOneButton.IsEnabled = false;
+            // Process something
+        }
+    }
+}
