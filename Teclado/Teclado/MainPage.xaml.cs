bea1bb152b9acd6e53fdca4d56e4b8bcd2f2f5c9
Juan Mejia <jumejia@microsoft.com>
2019-02-11 16:23:55 -0800

    Code Complete for Demo

--- Teclado/Teclado/MainPage.xaml.cs
+++ Teclado/Teclado/MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -11,19 +10,49 @@ namespace Teclado
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        
+        #region Stage 2
+        private bool preventLosingFocus = false;
+        #endregion
         public MainPage()
         {
-            InitializeComponent();          
+            InitializeComponent();
+
+            #region Stage 2
+            //FocusManager.LosingFocus += FocusManager_LosingFocus;
+            #endregion
         }
+
+        #region Stage 2
+        //private void FocusManager_LosingFocus(object sender, LosingFocusEventArgs e)
+        //{
+        //    if (preventLosingFocus)
+        //    {
+        //        e.Cancel = true;
+        //        preventLosingFocus = false;
+        //    }
+        //}
+        #endregion
 
         private void StepOneButton_Click(object sender, RoutedEventArgs e)
         {
+            #region Stage 2
+            //preventLosingFocus = true;
+            #endregion
+            
             StepOneButton.IsEnabled = false;
             // Process something
 
             ContainerOne.Visibility = Visibility.Collapsed;
             ContainerTwo.Visibility = Visibility.Visible;
+
+            #region Stage 2
+            //var next = FocusManager.FindNextElement(FocusNavigationDirection.Next);
+            //if (next.GetType() == typeof(Microsoft.UI.Xaml.Controls.NavigationViewItem))
+            //{
+            //    FocusManager.TryFocusAsync(EmailTextBox, FocusState.Programmatic);
+            //}
+            #endregion
         }
+
     }
 }
