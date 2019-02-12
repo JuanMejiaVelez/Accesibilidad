7786dc75ff19ac1a4f3c5f21ad98696752002eba
Juan Mejia <jumejia@microsoft.com>
2019-02-11 18:09:27 -0800

    Narrator Demo Complete

--- Narrador/Narrador/MainPage.xaml.cs
+++ Narrador/Narrador/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
@@ -22,9 +23,62 @@ namespace Narrador
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Stage 2
+        private bool preventLosingFocus = false;
+        #endregion
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            #region Stage 2
+            //FocusManager.LosingFocus += FocusManager_LosingFocus;
+            #endregion
         }
+
+        #region Stage 2
+        //private void FocusManager_LosingFocus(object sender, LosingFocusEventArgs e)
+        //{
+        //    if (preventLosingFocus)
+        //    {
+        //        e.Cancel = true;
+        //        preventLosingFocus = false;
+        //    }
+        //}
+        #endregion
+
+        private void StepOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region Stage 2
+            //preventLosingFocus = true;
+            #endregion
+
+            StepOneButton.IsEnabled = false;
+            // Process something
+
+            ContainerOne.Visibility = Visibility.Collapsed;
+            ContainerTwo.Visibility = Visibility.Visible;
+
+            #region Stage 2
+            //var next = FocusManager.FindNextElement(FocusNavigationDirection.Next);
+            //if (next.GetType() == typeof(Microsoft.UI.Xaml.Controls.NavigationViewItem))
+            //{
+            //    FocusManager.TryFocusAsync(EmailTextBox, FocusState.Programmatic);
+            //}
+            #endregion
+        }
+
+        private void StepTwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContainerThree.Visibility = Visibility.Visible;
+        }
+
+        //private void NotifyButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    AutomationPeer peer = ButtonAutomationPeer.CreatePeerForElement(NotifyButton);
+        //    if(peer != null)
+        //    {
+        //        peer.RaiseNotificationEvent(AutomationNotificationKind.ActionCompleted, AutomationNotificationProcessing.ImportantAll, "The notification button has been clicked", "NotifyButtonActivity");
+        //    }
+        //}
     }
 }
--- Teclado/Teclado/MainPage.xaml.cs
+++ Teclado/Teclado/MainPage.xaml.cs
@@ -53,6 +53,5 @@ namespace Teclado
             //}
             #endregion
         }
-
     }
 }
