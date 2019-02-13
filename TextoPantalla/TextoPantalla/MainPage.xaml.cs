362343d7cfc4eb6df2f967c8438445dfe08905b6
Juan Mejia <jumejia@microsoft.com>
2019-02-12 16:33:36 -0800

    Texto en Pantalla

--- TextoPantalla/TextoPantalla/MainPage.xaml.cs
+++ TextoPantalla/TextoPantalla/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +12,44 @@ namespace TextoPantalla
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private bool preventLosingFocus = false;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            FocusManager.LosingFocus += FocusManager_LosingFocus;
         }
-    }
+
+        private void FocusManager_LosingFocus(object sender, LosingFocusEventArgs e)
+        {
+            if (preventLosingFocus)
+            {
+                e.Cancel = true;
+                preventLosingFocus = false;
+            }
+        }
+
+        private void StepOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            preventLosingFocus = true;
+
+            StepOneButton.IsEnabled = false;
+            // Process something
+
+            ContainerOne.Visibility = Visibility.Collapsed;
+            ContainerTwo.Visibility = Visibility.Visible;
+
+            var next = FocusManager.FindNextElement(FocusNavigationDirection.Next);
+            if (next.GetType() == typeof(Microsoft.UI.Xaml.Controls.NavigationViewItem))
+            {
+                FocusManager.TryFocusAsync(EmailTextBox, FocusState.Programmatic);
+            }
+        }
+
+        private void StepTwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContainerThree.Visibility = Visibility.Visible;
+        }
+}
 }
