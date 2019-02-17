adf3e18e80f2900fc063c9fae84caaaed3fc3e73
Juan Mejia <jumejia@microsoft.com>
2019-02-17 13:41:07 -0800

    Correcciones de alineacion y cambio de nombre a la app Narrador

--- Narrador/Narrador/MainPage.xaml.cs
+++ Narrador/Narrador/MainPage.xaml.cs
@@ -4,7 +4,7 @@
 #endregion
 using Windows.UI.Xaml.Controls;
 #region Stage 2
-using Windows.UI.Xaml.Input;
+//using Windows.UI.Xaml.Input;
 #endregion
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -17,32 +17,32 @@ namespace Narrador
     public sealed partial class MainPage : Page
     {
         #region Stage 2
-        private bool preventLosingFocus = false;
+        //private bool preventLosingFocus = false;
         #endregion
         public MainPage()
         {
             InitializeComponent();
 
             #region Stage 2
-            FocusManager.LosingFocus += FocusManager_LosingFocus;
+            //FocusManager.LosingFocus += FocusManager_LosingFocus;
             #endregion
         }
 
         #region Stage 2
-        private void FocusManager_LosingFocus(object sender, LosingFocusEventArgs e)
-        {
-            if (preventLosingFocus)
-            {
-                e.Cancel = true;
-                preventLosingFocus = false;
-            }
-        }
+        //private void FocusManager_LosingFocus(object sender, LosingFocusEventArgs e)
+        //{
+        //    if (preventLosingFocus)
+        //    {
+        //        e.Cancel = true;
+        //        preventLosingFocus = false;
+        //    }
+        //}
         #endregion
 
         private void StepOneButton_Click(object sender, RoutedEventArgs e)
         {
             #region Stage 2
-            preventLosingFocus = true;
+            //preventLosingFocus = true;
             #endregion
 
             StepOneButton.IsEnabled = false;
@@ -52,13 +52,13 @@ namespace Narrador
             ContainerTwo.Visibility = Visibility.Visible;
 
             #region Stage 2
-            var next = FocusManager.FindNextElement(FocusNavigationDirection.Next);
-            if (next.GetType() == typeof(Microsoft.UI.Xaml.Controls.NavigationViewItem))
-            {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                FocusManager.TryFocusAsync(EmailTextBox, FocusState.Programmatic);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            }
+//            var next = FocusManager.FindNextElement(FocusNavigationDirection.Next);
+//            if (next.GetType() == typeof(Microsoft.UI.Xaml.Controls.NavigationViewItem))
+//            {
+//#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//                FocusManager.TryFocusAsync(EmailTextBox, FocusState.Programmatic);
+//#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//            }
             #endregion
         }
 
